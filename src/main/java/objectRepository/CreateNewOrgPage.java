0d4a0e849c3611aa36b0f3953774a2d185361442
asolkaryog <125435614+asolkaryog@users.noreply.github.com>
2023-07-08 20:19:52 +0530

    Update CreateNewOrgPage.java

--- src/main/java/objectRepository/CreateNewOrgPage.java
+++ src/main/java/objectRepository/CreateNewOrgPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CreateNewOrgPage {
-	//System.out.println("i am upadated from github TL")
+	//System.out.println("i am upadated from github TLlll")
 //declaration all element declred as privte
 	@FindBy(xpath ="")private WebElement orgNametxt;
 	@FindBy(xpath ="")private WebElement saveBtn;
